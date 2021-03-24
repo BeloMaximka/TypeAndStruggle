1cc02399c9b780cc60620e8010d0b3ee21a4fe08
BeloMaximka <belomaxim@gmail.com>
2021-03-24 14:18:41 +0200

    Упростил код в strpp.cpp

--- scr/strpp.cpp
+++ scr/strpp.cpp
@@ -28,21 +28,7 @@ void StrAddC(char*& StringPtr, const char Element, const int Index) {
 
 // Добавляет символ к концу строки
 void StrAddC(char*& StringPtr, const char Element) {
-	const int Index = strlen(StringPtr);
-	char* StringPtrSecond;
-	int StringPtrSecondSize = strlen(StringPtr) + 2;
-	StringPtrSecond = new char[StringPtrSecondSize];
-	for (int i = 0; i < Index; i++)
-	{
-		StringPtrSecond[i] = StringPtr[i];
-	}
-	StringPtrSecond[Index] = Element;
-	for (int i = Index + 1; i < StringPtrSecondSize; i++)
-	{
-		StringPtrSecond[i] = StringPtr[i - 1];
-	}
-	delete[] StringPtr;
-	StringPtr = StringPtrSecond;
+	StrAddC(StringPtr, Element, strlen(StringPtr));
 }
 
 // Удалячет символ со строки на определённом индексе
@@ -64,20 +50,7 @@ void StrRemoveC(char*& StringPtr, const int Index) {
 
 // Удаляет последний символ
 void StrRemoveC(char*& StringPtr) {
-	const int Index = strlen(StringPtr);
-	char* StringPtrSecond;
-	int StringPtrSecondSize = strlen(StringPtr) + 2;
-	StringPtrSecond = new char[StringPtrSecondSize];
-	for (int i = 0; i < Index; i++)
-	{
-		StringPtrSecond[i] = StringPtr[i];
-	}
-	for (int i = Index; i < StringPtrSecondSize; i++)
-	{
-		StringPtrSecond[i] = StringPtr[i + 1];
-	}
-	delete[] StringPtr;
-	StringPtr = StringPtrSecond;
+	StrRemoveC(StringPtr, strlen(StringPtr));
 }
 
 // Добавляет строку на указанный иденкс
@@ -105,26 +78,7 @@ void StrAddS(char*& StringPtrDest, const char* StringPtrScr, const int Index) {
 
 // Добавляет строку в конец уже имеющийся
 void StrAddS(char*& StringPtrDest, const char* StringPtrScr) {
-	const int Index = strlen(StringPtrDest);
-	char* StringPtrSecond;
-	int StringPtrSecondSize = strlen(StringPtrDest) + strlen(StringPtrScr) + 1;
-	StringPtrSecond = new char[StringPtrSecondSize];
-	for (int i = 0; i < Index; i++)
-	{
-		StringPtrSecond[i] = StringPtrDest[i];
-	}
-	for (int i = Index, j = 0; i < strlen(StringPtrDest) + strlen(StringPtrScr); i++)
-	{
-		StringPtrSecond[i] = StringPtrScr[j];
-		j++;
-	}
-	for (int i = Index + strlen(StringPtrScr); i < StringPtrSecondSize; i++)
-	{
-		StringPtrSecond[i] = StringPtrDest[i - strlen(StringPtrScr)];
-	}
-	StringPtrSecond[StringPtrSecondSize - 1] = '\0';
-	delete[] StringPtrDest;
-	StringPtrDest = StringPtrSecond;
+	StrAddS(StringPtrDest, StringPtrScr, strlen(StringPtrDest));
 }
 
 // Убирает Size символов на определённом индексе
